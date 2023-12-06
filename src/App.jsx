6cc9a20b6a4a53db284f251e99fdd55689a6dfac
Shahbaz Ali <shahbazalivk@gmail.com>
2023-12-07 02:56:21 +0500

    till shirt shadow done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Home from "./pages/Home";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 };
 
 export default App;
